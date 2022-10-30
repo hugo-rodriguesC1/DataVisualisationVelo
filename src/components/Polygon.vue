e93c933a9e1073db18bbc4c0ac1c2b108fed9ac2
Hugo Rodrigues <hugo.rodrigues@edu.univ-fcomte.fr>
2022-10-31 00:33:19 +0100

    visuel et test intersection observer

---
+++ src/components/Polygon.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+  width="213"
+  height="539"
+  viewBox="0 0 213 539"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  class="w-[213px] h-[539px]"
+  preserveAspectRatio="none"
+>
+  <path d="M0 112L28.75 15.0052H86.25L115 112L86.25 208.995H28.75L0 112Z" fill="#D9D9D9"></path>
+  <path
+    d="M98 218L126.75 121.005H184.25L213 218L184.25 314.995H126.75L98 218Z"
+    fill="#D9D9D9"
+  ></path>
+  <path
+    d="M98 427L126.75 330.005H184.25L213 427L184.25 523.995H126.75L98 427Z"
+    fill="#D9D9D9"
+  ></path>
+  <path d="M0 321L28.75 224.005H86.25L115 321L86.25 417.995H28.75L0 321Z" fill="#D9D9D9"></path>
+</svg>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,103 @@
+<script setup>
+import Polygon from "@/components/Polygon.vue";
+import {ref, onMounted} from 'vue'
+
+const observer = new IntersectionObserver((entries)=>{
+  for (const entry of entries) {
+    if(entry.isIntersecting){
+      entry.target.animate([
+        {transform: 'translateX(-100px)', opacity: 0},
+        {transform: 'translateX(0px)', opacity: 1},
+      ], {
+        duration :400
+      })
+    }
+  }
+})
+
+const div1 = ref(null)
+const div2 = ref(null)
+const div3 = ref(null)
+
+onMounted(()=>{
+ observer.observe(div1.value)
+ observer.observe(div2.value)
+ observer.observe(div3.value)
+})
+
+
+
+</script>
+
 <template>
-  <section>
-    <h1 class="text-2xl">
-      Bonjour<code class="font-mono text-base">/src/pages/index.vue</code>
-    </h1>
-  </section>
+  <div>
+    <div
+      class="flex h-screen w-screen flex-row items-center justify-between bg-[#13503A] px-96"
+    >
+      <div class="w-2/5">
+        <Polygon />
+      </div>
+      <div class="flex w-2/5 flex-col items-center">
+        <div class="text-xl font-normal text-gray-100">Hugo Rodrigues</div>
+        <h1 class="text-8xl font-bold leading-[130px] text-gray-100">
+          Aménagements d'infrastructures cyclables
+        </h1>
+      </div>
+    </div>
+
+    <div class="w-screen bg-gray-100 px-80 pt-44 text-zinc-900">
+      <h2 class="text-3xl font-bold">Introduction</h2>
+      <p class="mt-10 text-xl">
+        Aujourd’hui, le vélo ne représente que 3% des trajets. Le Plan vélo,
+        présenté par le Gouvernement en septembre 2018, vise à encourager le
+        développement de ce mode de déplacement en levant les principaux freins
+        identifiés. Il prévoit ainsi des actions réparties en quatre axes, parmi
+        lesquels l’amélioration de la sécurité routière par le développement
+        d’aménagements cyclables. L’objectif fixé était ainsi d’atteindre 55 477
+        km d’aménagements cyclables sécurisés (pistes cyclables et voies vertes)
+        en 2022.
+      </p>
+
+      <div class="flex flex-row mt-32 gap-20 items-center" ref="div1">
+        <div class="flex flex-col gap-10 flex-shrink">
+          <h2 class="text-3xl font-bold">Départements les plus concernés</h2>
+          <p class=" text-lg">
+            Aujourd’hui, le vélo ne représente que 3% des trajets. Le Plan vélo,
+            présenté par le Gouvernement en septembre 2018, vise à encourager le
+            développement de ce mode de déplacement en levant les principaux
+            freins identifiés.
+          </p>
+        </div>
+        <div class="bg-gray-500 flex-shrink-0 w-3/5 h-96 flex-grow"></div>
+      </div>
+
+
+      <div class="flex flex-row mt-32 gap-20 items-center" ref="div2">
+        <div class="bg-gray-500 flex-shrink-0 w-3/5 h-96 flex-grow"></div>
+        <div class="flex flex-col gap-10 flex-shrink">
+          <h2 class="text-3xl font-bold">Introduction</h2>
+          <p class=" text-lg">
+            Aujourd’hui, le vélo ne représente que 3% des trajets. Le Plan vélo,
+            présenté par le Gouvernement en septembre 2018, vise à encourager le
+            développement de ce mode de déplacement en levant les principaux
+            freins identifiés.
+          </p>
+        </div>
+      </div>
+
+
+      <div class="flex flex-row mt-32 gap-20 items-center" ref="div3">
+        <div class="flex flex-col gap-10 flex-shrink">
+          <h2 class="text-3xl font-bold">Introduction</h2>
+          <p class=" text-lg">
+            Aujourd’hui, le vélo ne représente que 3% des trajets. Le Plan vélo,
+            présenté par le Gouvernement en septembre 2018, vise à encourager le
+            développement de ce mode de déplacement en levant les principaux
+            freins identifiés.
+          </p>
+        </div>
+        <div class="bg-gray-500 flex-shrink-0 w-3/5 h-96 flex-grow"></div>
+      </div>
+    </div>
+  </div>
 </template>
